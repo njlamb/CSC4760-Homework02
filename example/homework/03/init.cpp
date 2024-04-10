9cedd16f8014a7ceec8b3c770efe3a11461e4d2f
njlamb <142761756+njlamb@users.noreply.github.com>
2024-04-09 22:27:48 -0500

    Update init.cpp for problem 3

--- example/homework/03/init.cpp
+++ example/homework/03/init.cpp
@@ -1,18 +1,40 @@
 #include <Kokkos_Core.hpp>
 #include <cstdio>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
   {
   // Make View of length n > 1000 and add values
+  const int n = 1000;
+
+  Kokkos::View<int*> view_0("view_0", n);
+  Kokkos::parallel_for("initialize_view_0", n, KOKKOS_LAMBDA(const int i) {
+    view_0(i) = i*i;
+  });
   
   // create two additional views of same size and datatype
+  Kokkos::View<int*> view_1("view_1", n);
+  Kokkos::View<int*> view_2("view_2", n);
  
   // deep_copy
-  
+  Kokkos::Timer timer;
+  Kokkos::deep_copy(view_1, view_0);
+  Kokkos::fence();
+  double time1 = timer.seconds();
+  timer.reset();
   // user copy
-
+  Kokkos::parallel_for("copy_view_0_to_view_2", n, KOKKOS_LAMBDA(const int i) {
+    view_2(i) = view_0(i);
+  });
+  Kokkos::fence();
+  double time2 = timer.seconds();
+  
   // Output times 
+  cout << "Deep copy time: " << time1 << endl;
+  cout << "Parallel_for copy time: " << time2 << endl;
   }
   Kokkos::finalize();
 }
